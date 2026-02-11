804f439368fc1150cac12a87a37a06d301d9d453
mirazahmeed <mirazahmed0619@gmail.com>
2026-02-11 14:36:36 +0600

    feat: Add form fields for job description, requirements, responsibilities, HR contact, and status, and remove a large commented-out JSON object.

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,54 +1,14 @@
 import React from "react";
 
 const AddJob = () => {
-	//     {
-	//     "_id": "698b39d9cf2501b37a0d90d7",
-	//     "jobId": "6988de62a9e366ebc901ecb4",
-	//     "applicantEmail": "hello@h.com",
-	//     "applicantName": null,
-	//     "applicantPhoto": null,
-	//     "linkedin": "http://localhost:5173/jobApply/6988de62a9e366ebc901ecb4",
-	//     "github": "http://localhost:5173/jobApply/6988de62a9e366ebc901ecb4",
-	//     "portfolio": "http://localhost:5173/jobApply/6988de62a9e366ebc901ecb4",
-	//     "resume": "http://localhost:5173/jobApply/6988de62a9e366ebc901ecb4",
-	//     "job": {
-	//       "_id": "6988de62a9e366ebc901ecb4",
-	//       "title": "Software Engineer",
-	//       "location": "Halishohor, Chittagong",
-	//       "jobType": "Hybrid",
-	//       "category": "Engineering",
-	//       "applicationDeadline": "2024-12-31",
-	//       "salaryRange": {
-	//         "min": 40000,
-	//         "max": 60000,
-	//         "currency": "bdt"
-	//       },
-	//       "description": "We are seeking a skilled Software Engineer to join our dynamic team. The candidate will work on diverse projects and contribute to innovative solutions.",
-	//       "company": "Favorite IT",
-	//       "requirements": [
-	//         "JavaScript",
-	//         "React",
-	//         "Node.js",
-	//         "MongoDB"
-	//       ],
-	//       "responsibilities": [
-	//         "Develop and maintain software",
-	//         "Collaborate with the team",
-	//         "Participate in code reviews"
-	//       ],
-	//       "status": "active",
-	//       "hr_email": "hr@techsolutions.com",
-	//       "hr_name": "Farhan Rahman",
-	//       "company_logo": "https://i.ibb.co/mXD5MNf/facebook.png"
-	//     }
-	//   },
-
 	return (
 		<div>
 			<h2 className="text-2xl font-bold">Add Job</h2>
 			<form action="">
 				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-					<legend className="fieldset-legend">Basic Information</legend>
+					<legend className="fieldset-legend">
+						Basic Information
+					</legend>
 
 					<label className="label">Job Title</label>
 					<input
@@ -124,7 +84,9 @@ const AddJob = () => {
 					</select>
 				</fieldset>
 				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-					<legend className="fieldset-legend">Application Deadline</legend>
+					<legend className="fieldset-legend">
+						Application Deadline
+					</legend>
 					<input type="date" className="input" />
 				</fieldset>
 
@@ -163,6 +125,60 @@ const AddJob = () => {
 							<option>GBP</option>
 						</select>
 					</div>
+				</fieldset>
+				{/* description */}
+				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+					<legend className="fieldset-legend">Description</legend>
+					<textarea
+						className="textarea h-24"
+						placeholder="Description"></textarea>
+					<div className="label">Optional</div>
+				</fieldset>
+				{/* Requirements */}
+				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+					<legend className="fieldset-legend">Requirements</legend>
+					<textarea
+						className="textarea h-24"
+						placeholder="Requirements"></textarea>
+					<div className="label">
+						Separate each requirement with a comma
+					</div>
+				</fieldset>
+				{/* Responsibilities */}
+				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+					<legend className="fieldset-legend">
+						Responsibilities
+					</legend>
+					<textarea
+						className="textarea h-24"
+						placeholder="Responsibilities"></textarea>
+					<div className="label">
+						Separate each responsibility with a comma
+					</div>
+				</fieldset>
+
+				{/* HR Email */}
+				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+					<legend className="fieldset-legend">HR Email</legend>
+					<input
+						type="email"
+						className="input"
+						placeholder="HR Email"
+					/>
+				</fieldset>
+				{/* HR Name */}
+				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+					<legend className="fieldset-legend">HR Name</legend>
+					<input
+						type="text"
+						className="input"
+						placeholder="HR Name"
+					/>
+				</fieldset>
+				{/* Status */}
+				<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+					<legend className="fieldset-legend">Status</legend>
+					<input type="checkbox" defaultChecked className="toggle" />
 				</fieldset>
 			</form>
 		</div>
